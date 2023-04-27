ceb77b4e1e585db58522170da5d570fd4287b6d3
pcyep7 <peacock.emma@icloud.com>
2023-04-27 18:59:24 +0100

    Commit

--- CLI.py
+++ CLI.py
@@ -1,5 +1,5 @@
 from PCA_utils.PCA_methods import perform_PCA, add_back_data
-from PCA_utils.graphs import scree_graph, loadings_graph, FG_graph, getPCA_plot, optimised_PCA_plot
+from PCA_utils.graphs import scree_graph, loadings_graph, getPCA_plot, optimised_PCA_plot
 from PCA_utils.reactionT import reactionT
 from PCA_utils.ReactionClass import reaction_class
 
@@ -26,11 +26,9 @@ scree_graph(cum_scree)
 # plot loading graph
 loadings_graph(loadings)
 
-# plot with functional groups highlighted
-FG_graph(principalDf)
-
 # plot optimised PCA graphs - title with user input?
 optimised_PCA_plot(principalDf, RC)
 
 # show plot with yield data for specified reaction class
 getPCA_plot(principalDf, "PCA", RC)
+
